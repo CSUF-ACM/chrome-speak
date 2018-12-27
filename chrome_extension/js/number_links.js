3545169441e5391a1f989c9e1f3bccfafc172c84
Zachary <zach.owen50@gmail.com>
2018-12-27 14:00:20 -0800

    Created a pop.js to send the message. Number_links.js recieved the message.

--- chrome_extension/js/number_links.js
+++ chrome_extension/js/number_links.js
@@ -1,19 +1,8 @@
-
 /*
-    Popup.html
+    
 */
-document.addEventListener('DOMContentLoaded', function() {
-    var number_links_btn = document.getElementById('number-links-btn');
 
-    number_links_btn.addEventListener('click', function() { // When the number links button is clicked on...
-            chrome.tabs.query({active: true}, function(tab_array) { // Tell the tab the user has currently opened to...
-                chrome.tabs.sendMessage(tab_array[0].id, {data: "a message"}, function(response) { // call the number links function in content scripts.
-                    
-                });
-            });
-    });
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    alert(message.command);
+//     //sendResponse({data: data, success: true});
 });
-
-/*
-    Content for website.
-*/
