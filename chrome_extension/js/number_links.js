2bff9103c9d8db8730317e42ac43cd8541ff7dec
Zachary <zach.owen50@gmail.com>
2018-12-30 04:55:13 -0800

    Hyperlink numbering is completed.

--- chrome_extension/js/number_links.js
+++ chrome_extension/js/number_links.js
@@ -3,10 +3,20 @@
 */
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if(message.command == "number links") {
-        var anchor = document.getElementsByTagName("a");
+        var anchor = document.querySelectorAll("a");
+        var link_number, text_node;
 
         for (var i = 0; i < anchor.length; ++i) {
-            anchor[i].innerHTML = (i + 1) + ' ' + anchor[i].innerHTML;
+            if (false) { // This statement is a temporary solution to prevent adding more and more numbers to a page that is already numbered.
+                
+            } else {
+                link_number = document.createElement("SPAN");
+                text_node = document.createTextNode(i + 1);
+                link_number.classList.add('chrome-speak-number-links');
+                link_number.appendChild(text_node);
+                anchor[i].insertBefore(link_number, anchor[i].firstChild);
+                console.log(anchor[i]);
+            }
         }
     }
 });
