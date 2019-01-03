071dcf326572963e17a7319200c8c888cc9a54fb
Zachary <zach.owen50@gmail.com>
2019-01-03 01:49:18 -0800

    added function to remove the enumerated links.

--- chrome_extension/js/number_links.js
+++ chrome_extension/js/number_links.js
@@ -1,22 +1,29 @@
+function remove_links() {
+    var number_links = document.querySelectorAll(".chrome-speak-number-links");
+        
+    for (var i = 0; i < number_links.length; ++i) {
+        number_links[i].remove();
+    }
+}
+
 /*
-    
+    Entry Point
 */
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    
     if(message.command == "number links") {
         var anchor = document.querySelectorAll("a");
         var link_number, text_node;
 
         for (var i = 0; i < anchor.length; ++i) {
-            if (false) { // This statement is a temporary solution to prevent adding more and more numbers to a page that is already numbered.
-                
-            } else {
-                link_number = document.createElement("SPAN");
-                text_node = document.createTextNode(i + 1);
-                link_number.classList.add('chrome-speak-number-links');
-                link_number.appendChild(text_node);
-                anchor[i].insertBefore(link_number, anchor[i].firstChild);
-                console.log(anchor[i]);
-            }
+            link_number = document.createElement("SPAN");
+            text_node = document.createTextNode(i + 1);
+            link_number.classList.add('chrome-speak-number-links');
+            link_number.appendChild(text_node);
+            anchor[i].insertBefore(link_number, anchor[i].firstChild);
         }
+
+    } else if (message.command == "cancel links") {
+        remove_links();
     }
 });
